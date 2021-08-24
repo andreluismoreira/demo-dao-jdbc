7ac3f44debaff6353c95c38277becdb88bb8eb80
andre luis <andreluistma@gmail.com>
2021-08-24 19:24:25 -0300

    implementação do findByDepartment

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,7 +1,10 @@
 package Application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,10 +13,18 @@ public class Program {
 		
 	SellerDao sellerDao = DaoFactory.createSellerDao();
 	
-	Seller seller = sellerDao.findById(3);
 	
+	System.out.println("================TESTE seller find by ID================");
+	Seller seller = sellerDao.findById(3);
 	System.out.println(seller);
-		
+	
+	System.out.println("================TESTE-2 seller find by DEPARTMENT================");
+	Department department = new Department(2, null);
+	List<Seller> depList = sellerDao.findByDepartment(department);
+	
+	for (Seller obj : depList) {
+		System.out.println(obj);
 	}
+ }
 
 }
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import DB.DB;
 import DB.DbException;
@@ -54,16 +57,9 @@ public class SellerDaoJdbc implements SellerDao{
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setDepartment(dep);
+				Department dep = instatiateDepartment(rs);
+				Seller obj = instatiateSeller(rs, dep);
+				
 				return obj;
 			}
 			return null;
@@ -79,11 +75,78 @@ public class SellerDaoJdbc implements SellerDao{
 			
 		}
 	}
-
+	
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+			
+		try {
+			
+			st = conn.prepareStatement("SELECT seller.*, department.DepName "
+					+ "FROM seller INNER JOIN department ON "
+					+ "seller.DepartmentId = department.Id "
+					+ "WHERE department.Id = ? "
+					+ "ORDER BY Name ");
+			
+			st.setInt( 1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer,Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					
+					dep = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instatiateSeller(rs, dep);
+				list.add(obj);
+			}
+		return list;
+		
+	} catch (SQLException e) {
+		
+		throw new DbException(e.getMessage());
+		
+	}finally {
+		
+		DB.closeStatement(st);
+		DB.closeResultSet(rs);
+	}
+	}
+	
+	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setDepartment(dep);
+		
+		return obj;
+	}
+
+	private Department instatiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+
+		return dep;
+	}
+
+	
 
 }
