cf25fa719e99060893a27f2f352580d0f2ac67f9
andre luis <andreluistma@gmail.com>
2021-08-24 10:44:02 -0300

    Criando Projeto e Repositorio

---
+++ src/DB/DBIntegrityException.java
@@ -0,0 +1,11 @@
+package DB;
+
+
+public class DBIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DBIntegrityException (String msg) {
+		super(msg);
+	}
+	
+}
